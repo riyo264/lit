50a0dd2e70a66cf07de91ea646c172860b213eed
Supriyo <138874454+riyo264@users.noreply.github.com>
2025-03-29 10:40:33 +0530

    Time: 0 ms (100%), Space: 9 MB (9.84%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -2,18 +2,32 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         int n = s.size();
-        int cnt = 0;
-        for(int i = n-1; i > -1; i--)
+        int temp = n-1;
+        int ans = 0;
+        int spcnt = 0;
+        stack <char> st;
+        for(int i=n-1; i>-1; i--)
         {
-            if(s[i]==' ' && (cnt != 0))
+            if(s[i] == ' ' && st.empty() == 1)
             {
+                temp--;
+                spcnt++;
+                continue;
+            }
+            else if(s[i] == ' ' && st.empty() == 0)
+            {
+                ans = temp - i;
                 break;
             }
-            else if(s[i]!=' ')
+            else if(i == 0 && s[i] != ' ')
             {
-                cnt++;
+                ans = n - spcnt;  
+            }
+            else if(s[i] != ' ')
+            {
+                st.push(s[i]);
             }
         }
-        return cnt;
+        return ans;
     }
 };
