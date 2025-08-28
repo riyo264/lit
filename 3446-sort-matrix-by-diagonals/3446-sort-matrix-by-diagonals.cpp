8c6eb4492015e7f761d39bcd2fbd97f10a363ec7
Supriyo <138874454+riyo264@users.noreply.github.com>
2025-08-29 00:09:26 +0530

    Time: 4 ms (86.01%), Space: 43.5 MB (65.43%) - LeetHub

--- 3446-sort-matrix-by-diagonals/3446-sort-matrix-by-diagonals.cpp
+++ 3446-sort-matrix-by-diagonals/3446-sort-matrix-by-diagonals.cpp
@@ -5,29 +5,54 @@ class Solution {
 public:
     vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-        unordered_map<int, vector<int>> diag;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                diag[i - j].push_back(grid[i][j]);
+        vector<vector<int>> ans(n, vector<int>(n, -1));
+        for(int i = 0; i < n; i++)
+        {
+            vector<int> temp;
+            int idx = i;
+            int j = 0;
+            int id = i;
+            int k = 0;
+            int l = 0;
+            while(idx < n && j < n) 
+            {
+                temp.push_back(grid[idx][j]);
+                idx++;
+                j++;
+            }
+            sort(temp.begin(), temp.end());
+            reverse(temp.begin(), temp.end());
+            while(id < n && k < n)
+            {
+                ans[id][k] = temp[l];
+                id++;
+                k++;
+                l++;
             }
         }
-        for(auto &entry : diag) {
-            int key = entry.first;
-            if(key >= 0) {
-                sort(entry.second.begin(), entry.second.end(), greater<int>());
-            } else {
-                sort(entry.second.begin(), entry.second.end());
+        for(int i = 1; i < n; i++)
+        {
+            vector<int> temp;
+            int idx = i;
+            int j = 0;
+            int id = i;
+            int k = 0;
+            int l = 0;
+            while(idx < n && j < n) 
+            {
+                temp.push_back(grid[j][idx]);
+                idx++;
+                j++;
+            }
+            sort(temp.begin(), temp.end());
+            while(id < n && k < n)
+            {
+                ans[k][id] = temp[l];
+                id++;
+                k++;
+                l++;
             }
         }
-
-        unordered_map<int,int> idx; 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                int key = i - j;
-                grid[i][j] = diag[key][idx[key]++];
-            }
-        }
-
-        return grid;
+        return ans;
     }
 };
