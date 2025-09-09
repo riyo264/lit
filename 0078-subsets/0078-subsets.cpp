e51d4e09e2a533c5f309497f1c99f14e72e3b2b2
Supriyo <138874454+riyo264@users.noreply.github.com>
2025-09-09 12:03:45 +0530

    Time: 0 ms (100%), Space: 10 MB (60.17%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,25 +1,42 @@
 class Solution {
+private:
+    void subseq(int n, int i, vector<int>& nums, vector<vector<int>>& ans, vector<int>& temp) {
+        if(i == n) {
+            ans.push_back(temp);
+            return;
+        } 
+        temp.push_back(nums[i]);
+        subseq(n, i+1, nums, ans,temp);
+        temp.pop_back();
+        subseq(n, i+1, nums, ans, temp);
+    }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n = (1<<nums.size());
-        vector<vector<int> > ans;
-        for(int i=0; i<n; i++)
-        {
-            vector<int> list;
-            for(int j=0; j<nums.size(); j++)
-            {
-                if(((i)&(1<<j))>0)
-                {
-                    list.push_back(nums[j]);
-                }
-                else
-                {
-                    continue;
-                }
-            }
-            ans.push_back(list);
-        }
+        int n = nums.size();
+        vector<vector<int>> ans;
+        vector<int> temp;
+        int i = 0;
+        subseq(n, i, nums, ans, temp);
         return ans;
+        // int n = (1<<nums.size());
+        // vector<vector<int> > ans;
+        // for(int i=0; i<n; i++)
+        // {
+        //     vector<int> list;
+        //     for(int j=0; j<nums.size(); j++)
+        //     {
+        //         if(((i)&(1<<j))>0)
+        //         {
+        //             list.push_back(nums[j]);
+        //         }
+        //         else
+        //         {
+        //             continue;
+        //         }
+        //     }
+        //     ans.push_back(list);
+        // }
+        // return ans;
     }
 
 };
