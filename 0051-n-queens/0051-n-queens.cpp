6a7cb7d731892f9f6ec3889d57fe4d899888902e
Supriyo <138874454+riyo264@users.noreply.github.com>
2025-10-20 19:51:56 +0530

    Time: 0 ms (100%), Space: 10.2 MB (74.59%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    void arrangements(vector<string>& queens, int n, int col, vector<vector<string>>& ans, vector<int>& leftcheck, vector<int>& upperdiagonal, vector<int>& lowerdiagonal) {
+        if(col == n) {
+            ans.push_back(queens);
+            return;
+        }
+
+        for(int r = 0; r < n; r++) 
+        {
+            if(leftcheck[r] == 0 && upperdiagonal[r+col] == 0 && lowerdiagonal[(n-1) + col - r] == 0) {
+                queens[r][col] = 'Q';
+                leftcheck[r] = 1;
+                upperdiagonal[r+col] = 1;
+                lowerdiagonal[(n-1) + col - r] = 1;
+                arrangements(queens, n, col+1, ans, leftcheck, upperdiagonal, lowerdiagonal);
+                queens[r][col] = '.';
+                leftcheck[r] = 0;
+                upperdiagonal[r+col] = 0;
+                lowerdiagonal[(n-1) + col - r] = 0;
+            }
+        }
+    }
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> queens(n, string(n, '.'));
+        vector<int> leftcheck(n, 0), upperdiagonal(2 * n - 1, 0), lowerdiagonal(2 * n - 1, 0);
+        arrangements(queens, n, 0, ans, leftcheck, upperdiagonal, lowerdiagonal);
+        return ans;
+    }
+};
