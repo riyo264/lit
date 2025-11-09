2180108328164713c5591e38e8de0b783837932c
Supriyo <138874454+riyo264@users.noreply.github.com>
2025-11-10 02:39:40 +0530

    Time: 0 ms (100%), Space: 8.5 MB (77.72%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        while(num >= 10) {
+            int x = num;
+            int sum = 0;
+            while(x > 0) {
+                sum += x % 10;
+                x /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+};
