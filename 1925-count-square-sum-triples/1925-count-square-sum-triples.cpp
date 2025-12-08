13df6c1f09cea5b13e520e59dde4b8ee05552175
Supriyo <138874454+riyo264@users.noreply.github.com>
2025-12-08 14:44:22 +0530

    Time: 3 ms (90.49%), Space: 7.8 MB (84.51%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -4,12 +4,12 @@ public:
         int cnt = 0;
         for(int i = 1; i <= n; i++)
         {
-            long long target = i*i;
-            long long lo = 1;
-            long long hi = target - 1;
-            while(lo <= hi) 
+            int target = i*i;
+            int lo = 1;
+            int hi = i - 1;
+            while(lo < hi) 
             {
-                long long x = ((1LL*lo*lo) + (1LL*hi*hi));
+                int x = lo * lo + hi * hi;
                 if(x == target) {
                     cnt += 2;
                     lo++;
