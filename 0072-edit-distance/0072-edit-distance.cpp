6ff35307e7d13e38e6cc7db09a310e679a10503c
Supriyo <138874454+riyo264@users.noreply.github.com>
2025-12-11 12:41:19 +0530

    Time: 68 ms (7.33%), Space: 84.3 MB (5.1%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int solve(string s, string t, vector<vector<int>>& dp, int i, int j) {
-        if(i < 0) return j+1;
-        if(j < 0) return i+1;
+        // To avoid negative index use 1-based indexing
+        if(i == 0) return j;  // for 0-based indexing "if(i < 0) return j+1"
+        if(j == 0) return i;  // same for this base case if 0-based indexing is used
         if(dp[i][j] != -1) {
             return dp[i][j];
         }
-        if(s[i] == t[j]) {
+        if(s[i-1] == t[j-1]) { // for 0-based indxing "s[i] == t[j]"
             return dp[i][j] = 0 + solve(s, t, dp, i-1, j-1);
         }
         return dp[i][j] = 1 + min(solve(s, t, dp, i-1, j), min(solve(s, t, dp, i, j-1), solve(s, t, dp, i-1, j-1)));
@@ -14,30 +15,32 @@ public:
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        // int ans = solve(word1, word2, dp, n-1, m-1);
-        // return ans;
-        vector<vector<int>> tab(n+1, vector<int>(m+1, 0));
-        for(int i = 0; i <= n; i++)
-        {
-            tab[i][0] = i;
-        }
-        for(int j = 0; j <= m; j++)
-        {
-            tab[0][j] = j;
-        }
-        for(int i = 1; i <= n; i++)
-        {
-            for(int j = 1; j <= m; j++)
-            {
-                if(word1[i-1] == word2[j-1]) {
-                    tab[i][j] = 0 + tab[i-1][j-1];
-                }
-                else {
-                    tab[i][j] = 1 + min(tab[i-1][j], min(tab[i][j-1], tab[i-1][j-1]));
-                }
-            }
-        }
-        return tab[n][m];
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1)); // n+1 & m+1 is for 1-based indexing
+        int ans = solve(word1, word2, dp, n, m);
+        return ans;
+
+        // Tabulation is in 1-based indexing
+        // vector<vector<int>> tab(n+1, vector<int>(m+1, 0));
+        // for(int i = 0; i <= n; i++)
+        // {
+        //     tab[i][0] = i;
+        // }
+        // for(int j = 0; j <= m; j++)
+        // {
+        //     tab[0][j] = j;
+        // }
+        // for(int i = 1; i <= n; i++)
+        // {
+        //     for(int j = 1; j <= m; j++)
+        //     {
+        //         if(word1[i-1] == word2[j-1]) {
+        //             tab[i][j] = 0 + tab[i-1][j-1];
+        //         }
+        //         else {
+        //             tab[i][j] = 1 + min(tab[i-1][j], min(tab[i][j-1], tab[i-1][j-1]));
+        //         }
+        //     }
+        // }
+        // return tab[n][m];
     }
 };
