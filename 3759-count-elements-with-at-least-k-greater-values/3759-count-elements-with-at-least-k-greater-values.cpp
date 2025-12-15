afa2a1d2e90fbead361c8be9991b3ec4684e0d7f
Supriyo <138874454+riyo264@users.noreply.github.com>
2025-12-16 01:53:37 +0530

    Time: 51 ms (85.61%), Space: 174 MB (52.66%) - LeetHub

--- 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.cpp
+++ 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     int countElements(vector<int>& nums, int k) {
-         int n = nums.size();
+        int n = nums.size();
         sort(nums.begin(), nums.end());
-        
         int ans = 0;
-        for (int i = 0; i < n; i++) {
-            int greater = n - (upper_bound(nums.begin(), nums.end(), nums[i]) - nums.begin());
-            if (greater >= k) ans++;
+        if(k == 0) {
+            return n;
         }
-
+        int maxi = nums[n-k];
+        for(int x : nums) {
+            if(x < maxi) ans++;
+            else {
+                break;
+            }
+        }
         return ans;
     }
 };
