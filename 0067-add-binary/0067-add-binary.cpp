ee9c452169fa22a2f1df0265faae875acc7cef3a
Supriyo <138874454+riyo264@users.noreply.github.com>
2026-02-15 11:49:02 +0530

    Time: 0 ms (100%), Space: 9 MB (59.48%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,55 +1,21 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int n = max(a.size(), b.size());
-        string ans;
-        while(a.size() != b.size()) 
-        {
-            if(a.size() < b.size()) {
-                a = '0' + a;
-            }
-            else {
-                b = '0' + b;
-            }
+        int i = a.size() - 1;
+        int j = b.size() - 1;
+        int carry = 0;
+        string result = "";
+
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+            if (i >= 0) sum += a[i--] - '0';
+            if (j >= 0) sum += b[j--] - '0';
+
+            result.push_back((sum % 2) + '0');
+            carry = sum / 2;
         }
-        bool carry = false;
-        for(int i = n-1; i > -1; i--)
-        {
-            if(a[i] == '1' && b[i] == '1') {
-                if(!carry) {
-                    ans.push_back('0');
-                    carry = true;
-                }
-                else {
-                    ans.push_back('1');
-                    carry = true;
-                }
-            }
-            else if(a[i] == '1' || b[i] == '1') {
-                if(!carry) {
-                    ans.push_back('1');
-                    carry = false;
-                }
-                else {
-                    ans.push_back('0');
-                    carry = true;
-                }
-            }
-            else {
-                if(!carry) {
-                    ans.push_back('0');
-                    carry = false;
-                }
-                else {
-                    ans.push_back('1');
-                    carry = false;
-                }
-            }
-        }
-        if(carry) {
-            ans.push_back('1');
-        }
-        reverse(ans.begin(), ans.end());
-        return ans;
+
+        reverse(result.begin(), result.end());
+        return result;
     }
 };
