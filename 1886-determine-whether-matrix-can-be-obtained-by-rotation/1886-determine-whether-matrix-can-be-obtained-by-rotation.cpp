d95e74926171bbb2f76e95702a7339f9b5e8d4cb
Supriyo <138874454+riyo264@users.noreply.github.com>
2026-03-22 10:49:50 +0530

    Time: 8 ms (0.35%), Space: 15 MB (14.32%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -4,9 +4,18 @@ public:
         int n = mat.size();
         int m = mat[0].size();
         vector<vector<int>> temp(n, vector<int>(m));
-        for(int k = 0; k < 4; k++)
+        if(mat == target) {
+            return true;
+        } 
+        for(int k = 0; k < 3; k++)
         {
-            temp = mat;
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < m; j++)
+                {
+                    temp[j][(m-1)-i] = mat[i][j];
+                }
+            }
             bool match = true;
             for(int i = 0; i < n; i++)
             {
@@ -25,13 +34,6 @@ public:
             }
             if(match) {
                 return true;
-            }
-            for(int i = 0; i < n; i++)
-            {
-                for(int j = 0; j < m; j++)
-                {
-                    temp[j][(m-1)-i] = mat[i][j];
-                }
             }
             mat = temp;
         }
