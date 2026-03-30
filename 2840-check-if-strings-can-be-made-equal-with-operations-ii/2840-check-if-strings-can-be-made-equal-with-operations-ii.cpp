f89c540222ac5020d0ebcd51162c3f30cc98795f
Supriyo <138874454+riyo264@users.noreply.github.com>
2026-03-30 19:20:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -5,18 +5,23 @@ public:
         map<char, int> mpp1, mpp2;
         for(int i = 0; i < n; i += 2) {
             mpp1[s1[i]]++;
-            mpp2[s2[i]]++;
+            mpp1[s2[i]]--;
         }
-        if(mpp1 != mpp2) {
-            return false;
+        for(auto x : mpp1) 
+        {
+            if(x.second != 0) {
+                return false;
+            }
         }
-        map<char, int> mpp_o, mppt;
         for(int i = 1; i < n; i += 2) {
-            mpp_o[s1[i]]++;
-            mppt[s2[i]]++;
+            mpp2[s1[i]]++;
+            mpp2[s2[i]]--;
         }
-        if(mpp_o != mppt) {
-            return false;
+        for(auto x : mpp2) 
+        {
+            if(x.second != 0) {
+                return false;
+            }
         }
         return true;
     }
