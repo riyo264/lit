d189e22303d06256993cf264a64153dcecf55573
Supriyo <138874454+riyo264@users.noreply.github.com>
2026-04-02 02:47:25 +0530

    Time: 0 ms (100%), Space: 8.9 MB (51.74%) - LeetHub

---
+++ 3794-reverse-string-prefix/3794-reverse-string-prefix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string reversePrefix(string s, int k) {
+        int i = 0;
+        int j = k-1;
+        while(i < j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+        return s;
+    }
+};
