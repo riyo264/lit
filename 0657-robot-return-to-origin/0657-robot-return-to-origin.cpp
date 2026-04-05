d7a734a2694b794b1217fe7e260fe3da75006cf2
Supriyo <138874454+riyo264@users.noreply.github.com>
2026-04-05 12:10:18 +0530

    Time: 4 ms (27.83%), Space: 10.3 MB (76.78%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int n = moves.size();
+        int x = 0, y = 0;
+        bool pos = true;
+        for(int i = 0; i < n; i++)
+        {
+            if(moves[i] == 'L') {
+                x--;
+            }
+            else if(moves[i] == 'R') {
+                x++;
+            }
+            else if(moves[i] == 'U') {
+                y++;
+            }
+            else {
+                y--;
+            }
+        }
+        return (0 == x && 0 == y);
+    }
+};
