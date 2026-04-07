16e846a6849c8add43960ea66cbd40f5557baf3d
Supriyo <138874454+riyo264@users.noreply.github.com>
2026-04-08 04:38:27 +0530

    Time: 2591 ms (5.57%), Space: 310.6 MB (7.05%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -5,33 +5,36 @@ public:
         int m = words.size();
         int k = words[0].size();
         int limit = m*k;
-        int i = 0, j = limit-1;
         vector<int> ans;
         unordered_map<string, int> mpp1;
         for(string it : words) 
         {
             mpp1[it]++;
         }
-        while(j < n) {
-            unordered_map<string, int> mpp;
-            string t = "";
-            for(int a = i; a <= j+1; a++)
-            {
-                if(t.size() == k) {
-                    mpp[t]++;
-                    if(a < n) {
-                        t = s[a];
+        for(int offset = 0; offset < k; offset++) {
+            int i = offset;
+            int j = i+limit-1;
+            while(j < n) {
+                unordered_map<string, int> mpp;
+                string t = "";
+                for(int a = i; a <= j+1; a++)
+                {
+                    if(t.size() == k) {
+                        mpp[t]++;
+                        if(a < n) {
+                            t = s[a];
+                        }
+                    }
+                    else {
+                        t += s[a];
                     }
                 }
-                else {
-                    t += s[a];
+                if(mpp == mpp1) {
+                    ans.push_back(i);
                 }
+                i += k;
+                j += k;
             }
-            if(mpp == mpp1) {
-                ans.push_back(i);
-            }
-            i += k;
-            j += k;
         }
         return ans;
     }
