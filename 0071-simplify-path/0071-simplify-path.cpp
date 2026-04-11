f3e8b3cbf9754cfd9c07e80da1b0e6594c2f762d
Supriyo <138874454+riyo264@users.noreply.github.com>
2026-04-11 15:46:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,88 +1,116 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        int n = path.size();
-        stack<char> st;
-        int dot_cnt = 0;
-        for(int i = 0; i < n; i++)
-        {
-            if(path[i] == '.' && (st.top() == '/' || dot_cnt != 0)) {
-                dot_cnt++;
-                if(dot_cnt > 2) {
-                    dot_cnt = 0;
-                }
-            }
-            else if(path[i] != '/') {
-                dot_cnt = 0;
-            }
-            else if(path[i]=='/' && dot_cnt == 1) {
-                if(st.size() >= 2) {
-                    st.pop();
-                    st.pop();
-                }
-                else {
-                    while(!st.empty()) {
-                        st.pop();
-                    }
-                }
-                dot_cnt = 0;
-            }
-            else if(path[i] == '/' && dot_cnt == 2) {
-                if(st.size() >= 3) {
-                    st.pop();
-                    st.pop();
-                    st.pop();
-                }
-                while(!st.empty() && st.top() != '/') {
-                    st.pop();
-                }
-                if(!st.empty()) {
-                    st.pop();
-                }
-                dot_cnt = 0;
-            }
-            else if(!st.empty() && path[i] == '/' && st.top() == '/') {
+        // int n = path.size();
+        // stack<char> st;
+        // int dot_cnt = 0;
+        // for(int i = 0; i < n; i++)
+        // {
+        //     if(path[i] == '.' && (st.top() == '/' || dot_cnt != 0)) {
+        //         dot_cnt++;
+        //         if(dot_cnt > 2) {
+        //             dot_cnt = 0;
+        //         }
+        //     }
+        //     else if(path[i] != '/') {
+        //         dot_cnt = 0;
+        //     }
+        //     else if(path[i]=='/' && dot_cnt == 1) {
+        //         if(st.size() >= 2) {
+        //             st.pop();
+        //             st.pop();
+        //         }
+        //         else {
+        //             while(!st.empty()) {
+        //                 st.pop();
+        //             }
+        //         }
+        //         dot_cnt = 0;
+        //     }
+        //     else if(path[i] == '/' && dot_cnt == 2) {
+        //         if(st.size() >= 3) {
+        //             st.pop();
+        //             st.pop();
+        //             st.pop();
+        //         }
+        //         while(!st.empty() && st.top() != '/') {
+        //             st.pop();
+        //         }
+        //         if(!st.empty()) {
+        //             st.pop();
+        //         }
+        //         dot_cnt = 0;
+        //     }
+        //     else if(!st.empty() && path[i] == '/' && st.top() == '/') {
+        //         continue;
+        //     }
+        //     st.push(path[i]);
+        // }
+        // if(!st.empty() && st.top() == '.') {
+        //     if(dot_cnt== 1) {
+        //         if(st.size() > 2) {
+        //             st.pop();
+        //             st.pop();
+        //         }
+        //         else {
+        //             st.pop();
+        //         }
+        //         dot_cnt = 0;
+        //     }
+        //     else if(dot_cnt == 2) {
+        //         if(st.size() >= 3) {
+        //             st.pop();
+        //             st.pop();
+        //             st.pop();
+        //         }
+        //         while(!st.empty() && st.top() != '/') {
+        //             st.pop();
+        //         }
+        //         if(!st.empty()) {
+        //             st.pop();
+        //         }
+        //         dot_cnt = 0;
+        //     }
+        // }
+        // string ans;
+        // while(!st.empty()) {
+        //     ans.push_back(st.top());
+        //     st.pop();
+        // }
+        // reverse(ans.begin(), ans.end());
+        // if(ans.size() == 0) {
+        //     ans = "/";
+        // }
+        // if(ans.size() > 1 && ans.back() == '/') {
+        //     ans.pop_back();
+        // }
+        // return ans;
+
+        string st[3005];
+        int top  = 0 ;
+        stringstream ss(path) ;
+        string token ;
+        while (getline(ss,token,'/')) {
+            if (token==""||token==".") {
                 continue;
             }
-            st.push(path[i]);
-        }
-        if(!st.empty() && st.top() == '.') {
-            if(dot_cnt== 1) {
-                if(st.size() > 2) {
-                    st.pop();
-                    st.pop();
+            else if (token=="..") {
+                if (top>0) {
+                    top--;
                 }
-                else {
-                    st.pop();
-                }
-                dot_cnt = 0;
             }
-            else if(dot_cnt == 2) {
-                if(st.size() >= 3) {
-                    st.pop();
-                    st.pop();
-                    st.pop();
-                }
-                while(!st.empty() && st.top() != '/') {
-                    st.pop();
-                }
-                if(!st.empty()) {
-                    st.pop();
-                }
-                dot_cnt = 0;
+            else {
+                st[top] = token;
+                top++;
             }
         }
         string ans;
-        while(!st.empty()) {
-            ans.push_back(st.top());
-            st.pop();
+        if (top==0) {
+            ans +="/";
+            return ans;
         }
-        reverse(ans.begin(), ans.end());
-        if(ans.size() == 0) {
-            ans = "/";
-        }
-        if(ans.size() > 1 && ans.back() == '/') {
-            ans.pop_back();
+        for (int i = 0; i < top ;i++) {
+            ans+="/"+st[i];
         }
         return ans;
     }
