8969ae3a1db5151717971d074d12489944b39ecc
Supriyo <138874454+riyo264@users.noreply.github.com>
2026-06-08 09:14:18 +0530

    Time: 7 ms (62.22%), Space: 133.5 MB (43.07%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -3,26 +3,23 @@ public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         int n = nums.size();
         vector<int> ans;
-        vector<int> lar;
-        int cnt = 0;
         for(int i = 0; i < n; i++)
         {
             if(nums[i] < pivot) {
                 ans.push_back(nums[i]);
             }
-            else if(nums[i] > pivot) {
-                lar.push_back(nums[i]);
-            }
-            else {
-                cnt++;
-            }
         }
-        while(cnt--) {
-            ans.push_back(pivot);
-        }
-        for(int i = 0; i < lar.size(); i++)
+        for(int i = 0; i < n; i++)
         {
-            ans.push_back(lar[i]);
+            if(nums[i] == pivot) {
+                ans.push_back(nums[i]);
+            }
+        }
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] > pivot) {
+                ans.push_back(nums[i]);
+            }
         }
         return ans;
     }
